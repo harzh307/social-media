b1c4df679a5be454c41a3c72e1341589762af582
harzh307 <harzh.patel@gmail.com>
2024-01-28 20:59:55 +0530

    commit code

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,53 @@
+// @ts-ignore
+import bcrypt from "bcryptjs";
+// @ts-ignore
+import jwt from "jsonwebtoken";
+import { NextResponse, NextRequest } from "next/server";
+import "dotenv/config";
+import User from "../../../provider/models/User";
+import connectToDatabase from "../../../provider/connectToDb";
+
+export const POST = async (req: NextRequest, res: NextResponse) => {
+  try {
+    const db = connectToDatabase();
+    const { email, password } = await req?.json();
+    if (!email) {
+      return Response.json("Email is required");
+    }
+    if (!password) {
+      return Response.json("Password is required");
+    }
+    const existingUser = await User.findOne({
+      email,
+    }).select("+password");
+    if (!existingUser) {
+      return Response.json(
+        "User does not exist with this same email, try different email insted"
+      );
+    }
+    const isPasswordCorrect = bcrypt.compareSync(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordCorrect) {
+      return Response.json({ message: "Incorrect password" });
+    }
+    const tokenPayload = {
+      userId: existingUser._id,
+      email: existingUser.email,
+    };
+    const options = {};
+    const user = await User.findById(existingUser._id);
+
+    const token = jwt.sign(tokenPayload, "token", options);
+    return Response.json({
+      user,
+      token,
+      message: "user created successfully",
+    });
+  } catch (error: any) {
+    return Response.json({
+      message: error.message,
+    });
+  }
+};
---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,58 @@
+// @ts-ignore
+import bcrypt from "bcryptjs";
+// @ts-ignore
+import jwt from "jsonwebtoken";
+import { NextResponse, NextRequest } from "next/server";
+import "dotenv/config";
+import User from "../../../provider/models/User";
+import connectToDatabase from "../../../provider/connectToDb";
+
+export const POST = async (req: NextRequest, res: NextResponse) => {
+  try {
+    const db = connectToDatabase();
+    const { email, password, name } = await req?.json();
+    if (!email) {
+      return Response.json("Email is required");
+    }
+    if (!password) {
+      return Response.json("Password is required");
+    }
+    if (!name) {
+      return Response.json("Name is required");
+    }
+    const existingUser = await User.findOne(
+      {
+        email,
+      },
+      {}
+    );
+    if (existingUser) {
+      return Response.json(
+        "User already exist with the same email, login insted"
+      );
+    }
+    const hashPassword = bcrypt.hashSync(password);
+    const user = new User({
+      email,
+      name,
+      password: hashPassword,
+    });
+    await user.save();
+    const tokenPayload = {
+      userId: user._id,
+      email: user.email,
+    };
+    const options = {};
+
+    const token = jwt.sign(tokenPayload, "token", options);
+    return Response.json({
+      user,
+      token,
+      message: "user created successfully",
+    });
+  } catch (error) {
+    return Response.json({
+      message: error.message,
+    });
+  }
+};
